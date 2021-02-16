c476dbac8264069ea346af5c38fe9819ab7157af
Curtis H <sitrucrettah1@gmail.com>
2021-02-15 22:11:24 -0800

    Delete Database

--- db/schema.sql
+++
@@ -1,9 +0,0 @@
-DROP DATABASE IF EXISTS burgers_db;
-CREATE DATABASE burgers_db;
-USE burgers_db;
-CREATE TABLE burgers(
-  id AUTO_INCREMENT NOT NULL,
-  burger_name VARCHAR(100),
-  devoured BOOLEAN DEFAULT FALSE,
-  PRIMARY KEY (id)
-);
